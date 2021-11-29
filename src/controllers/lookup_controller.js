e52876a910ac382f9608bbb0149367a33aebce49
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-11-29 16:39:42 +0530

    added a rest api to add info to lookup-menu

--- src/controllers/lookup_controller.js
+++ src/controllers/lookup_controller.js
@@ -2,10 +2,6 @@ const lookupService = require("../services/lookup_service");
 const carrerList = require("../constants/CarrerField");
 const LookUpModel = require("../model/lookup_model");
 
-const carress = [
-  { name: "Data science internship", lookup_type: "Carrer" },
-  { name: "Data manager internship", lookup_type: "Carrer" },
-];
 const getLookupDetails = async (req, res) => {
   console.log("getLookupDetails");
   try {
@@ -20,20 +16,6 @@ const getLookupDetails = async (req, res) => {
 const postCarrerOptions = async (req, res) => {
   const list = [];
   try {
-    // carrerList.map(async (carrer) => {
-    //   try {
-    //     const newCarrer = new LookUpModel({
-    //       name: carrer.name,
-    //       lookup_type: carrer.lookup_type,
-    //     });
-    //     const savedCarrer = await newCarrer.save();
-    //     list.push(savedCarrer);
-    //     res.status(200).josn("zalee");
-    //   } catch (error) {
-    //     return res.status(500).json(error);
-    //   }
-    // });
-    // try {
     var i = 0;
     while (i < carrerList.length) {
       const newCarrer = new LookUpModel({
@@ -45,10 +27,7 @@ const postCarrerOptions = async (req, res) => {
       i++;
       //   return res.status(200).json(savedCarrer);
     }
-    // } catch (error) {
-    //   res.status(500).json(error);
-    // }
-    // addCarrers();
+
     return res.status(200).json(list);
   } catch (error) {
     return res.status(500).json(error);
