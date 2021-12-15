c62b0a02f21a258da8d6102f4056ca76becfc341
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-12-15 14:57:33 +0530

    --done

--- src/routes/user_route.js
+++ src/routes/user_route.js
@@ -1,12 +1,33 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-let func = require('../config/function');
-const userController = require('../controllers/user_controller');
-router.post(func.urlCons.URL_USER_REGISTRATION, userController.userRegistration);
+let func = require("../config/function");
+let userModel = require("../model/user_model");
+const userController = require("../controllers/user_controller");
+router.post(
+  func.urlCons.URL_USER_REGISTRATION,
+  userController.userRegistration
+);
 router.post(func.urlCons.URL_USER_LOGIN, userController.userLogin);
 router.post(func.urlCons.URL_LINKED_LOGIN, userController.linkedInLogin);
 router.get(func.urlCons.URL_USER_LIST, userController.getUserList);
-router.post(func.urlCons.URL_TEMP_USER_DATA_SAVE, userController.tempUserRegister);
-router.post(func.urlCons.URL_GET_TEMP_USER_DATA, userController.getTempUserData);
-router.post(func.urlCons.URL_UPDATE_USER_REGISTRATION_STATUS, userController.updateUserRegisterStatus);
+router.post(
+  func.urlCons.URL_TEMP_USER_DATA_SAVE,
+  userController.tempUserRegister
+);
+router.post(
+  func.urlCons.URL_GET_TEMP_USER_DATA,
+  userController.getTempUserData
+);
+router.post(
+  func.urlCons.URL_UPDATE_USER_REGISTRATION_STATUS,
+  userController.updateUserRegisterStatus
+);
+router.get("/applicant/:id", async (req, res) => {
+  try {
+    const fetchedUser = await userModel.find({ _id: req.params.id });
+    return res.status(200).json(fetchedUser);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 module.exports = router;
