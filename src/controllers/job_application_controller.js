994ae2f559d6a638e0b949340aabf38ddb9ed2f7
Alroy Fernandes <alroyfernandes07518@gmail.com>
2022-01-12 17:14:23 +0530

    created an restapi endpoint to check number of jobs the given candidate has applied to

--- src/controllers/job_application_controller.js
+++ src/controllers/job_application_controller.js
@@ -1,35 +1,48 @@
-const jobApplicationService = require('../services/job_application_service');
-const func = require('../config/function');
+const jobApplicationService = require("../services/job_application_service");
+const func = require("../config/function");
+const jobModel = require("../model/job_model");
 const applyJob = async (req, res) => {
-    try {
-        const applyJobData = await jobApplicationService.applyJobService(req.body)
-        console.log(applyJobData)
-        return res.send(applyJobData)
-    } catch (err) {
-        return res.send(err)
-    }
-
-}
+  try {
+    const applyJobData = await jobApplicationService.applyJobService(req.body);
+    console.log(applyJobData);
+    return res.send(applyJobData);
+  } catch (err) {
+    return res.send(err);
+  }
+};
 const appliedJobStatus = async (req, res) => {
-    try {
-        const applyJobData = await jobApplicationService.appliedJobStatusService(req.body)
-        console.log(applyJobData)
-        return res.send(applyJobData)
-    } catch (err) {
-        return res.send(err)
-    }
-
-}
+  try {
+    const applyJobData = await jobApplicationService.appliedJobStatusService(
+      req.body
+    );
+    console.log(applyJobData);
+    return res.send(applyJobData);
+  } catch (err) {
+    return res.send(err);
+  }
+};
 const getUserByJob = async (req, res) => {
-    try {
-        const getJobData = await jobApplicationService.getUserDetailsByJobService(req)
-        console.log(getJobData)
-        return res.send(getJobData)
-    } catch (err) {
-        return res.send(err)
-    }
+  try {
+    const getJobData = await jobApplicationService.getUserDetailsByJobService(
+      req
+    );
+    console.log(getJobData);
+    return res.send(getJobData);
+  } catch (err) {
+    return res.send(err);
+  }
+};
 
-}
+const getApplicant = async (req, res) => {
+  try {
+    const applicant = await jobModel.find({ candidate_id: req.params.id });
+
+    const isPresent = applicant.length !== 0;
+    return res.status(200).json(isPresent);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
 // const getJob = async (req, res) => {
 //     try {
 //         const getJobData = await jobApplicationService.getJobService(req)
@@ -52,4 +65,4 @@ const getUserByJob = async (req, res) => {
 
 // }
 
-module.exports = { applyJob, appliedJobStatus,getUserByJob }
+module.exports = { applyJob, appliedJobStatus, getUserByJob, getApplicant };
