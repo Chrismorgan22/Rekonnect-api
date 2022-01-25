a5a8aec2d97774f9264e6ce024d273afa6b4ff20
Alroy Fernandes <alroyfernandes07518@gmail.com>
2022-01-25 20:22:31 +0530

    created rest api to sort records

--- src/routes/lookup_route.js
+++ src/routes/lookup_route.js
@@ -1,8 +1,22 @@
 let express = require("express");
 let router = express.Router();
+let lookupModel = require("../model/lookup_model");
 let func = require("../config/function");
 const lookupController = require("../controllers/lookup_controller");
 router.post(func.urlCons.URL_GET_LOOKUP, lookupController.getLookupDetails);
 
 router.post(func.urlCons.URL_STORE_LOOKUP, lookupController.postCarrerOptions);
+
+// router.post("/sorted", async (req, res) => {
+//   try {
+//     const sortedFields = await lookupModel
+//       .find({
+//         lookup_type: req.body.lookup_type,
+//       })
+
+//     return res.status(200).json(sortedFields);
+//   } catch (error) {
+//     return res.status(500).json(error);
+//   }
+// });
 module.exports = router;
--- src/services/lookup_service.js
+++ src/services/lookup_service.js
@@ -1,32 +1,29 @@
-const func = require('../config/function');
-const LookupSchema = require('../model/lookup_model');
+const func = require("../config/function");
+const LookupSchema = require("../model/lookup_model");
 
 const getCountryService = async (body) => {
-    return new Promise(async (resolve, reject) => {
-        let query = {};
+  return new Promise(async (resolve, reject) => {
+    let query = {};
 
-        query = {
-            $and: [
-                { is_deleted: false },
-                { lookup_type: body.lookup_type },
-            ]
-        }
+    query = {
+      $and: [{ is_deleted: false }, { lookup_type: body.lookup_type }],
+    };
 
-        await LookupSchema.find(query, function (err, docs) {
-            console.log(err, docs);
-            if (err) {
-                func.msCons.errorJson["message"] = "Error in retrieving data";
-                func.msCons.errorJson["error"] = err;
-                return resolve(func.msCons.errorJson);
-            } else if (!docs || docs.length === 0) {
-                func.msCons.errorJson["message"] = "Error in retrieving data";
-                func.msCons.errorJson["error"] = err;
-                return resolve(func.msCons.errorJson);
-            } else {
-                func.msCons.successJson['data'] = docs;
-                return resolve(func.msCons.successJson)
-            }
-        });
-    })
-}
-module.exports = { getCountryService }
+    await LookupSchema.find(query, function (err, docs) {
+      console.log(err, docs);
+      if (err) {
+        func.msCons.errorJson["message"] = "Error in retrieving data";
+        func.msCons.errorJson["error"] = err;
+        return resolve(func.msCons.errorJson);
+      } else if (!docs || docs.length === 0) {
+        func.msCons.errorJson["message"] = "Error in retrieving data";
+        func.msCons.errorJson["error"] = err;
+        return resolve(func.msCons.errorJson);
+      } else {
+        func.msCons.successJson["data"] = docs;
+        return resolve(func.msCons.successJson);
+      }
+    }).sort({ name: 1 });
+  });
+};
+module.exports = { getCountryService };
