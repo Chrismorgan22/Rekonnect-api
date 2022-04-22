e157dac2f69caadc10bf216ad79b22eba0e82b63
Alroy Fernandes <alroyfernandes07518@gmail.com>
2022-04-22 14:01:17 +0530

    fixe the schema

--- src/routes/mentor_route.js
+++ src/routes/mentor_route.js
@@ -4,4 +4,6 @@ let mentor_controller = require("../controllers/mentor_controller");
 
 router.post(urlCons.URL_MENTOR_REGISTRATION, mentor_controller.registerMentor);
 router.get(urlCons.URL_MENTOR_FETCH, mentor_controller.fetchMentor);
+//
+router.post(urlCons.URL_MENTOR_BOOKING_ADD, mentor_controller.addBooking);
 module.exports = router;
